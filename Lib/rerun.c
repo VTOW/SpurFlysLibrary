cd46b583d9e4b35e572a31e2f01983e8d2058f80
SpurFlys <SpurFlys@users.noreply.github.com>
2016-12-04 18:34:01 -0800

    Updated comments

--- Lib/rerun.c
+++ Lib/rerun.c
@@ -1,3 +1,9 @@
+//This code would need to be modified
+//slightly to suit your robot
+
+//The logic shouldn't change,
+//just some variable names
+
 #define RIGHTARM I2C_1
 #define LEFTARM  I2C_2
 #define RIGHTCHASSIS I2C_3
@@ -7,7 +13,7 @@ task
 record () {
 	clearDebugStream();
 
-	writeDebugStream("\nautonomous[150][3] = {");
+	writeDebugStream("\nint autonomous[150][3] = {");
 
 	for (int i = 0; i < 150; i++) {
 		int arm = (SensorValue[RIGHTARM] + -SensorValue[LEFTARM]) / 2;
